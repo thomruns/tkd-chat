dbf14cff8437e8b8a27e68d97ea53200cca18b7b
Thom David <thomdavid74@gmail.com>
2018-10-16 11:07:57 -0700

    Chat component and route

---
+++ src/components/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chat container">
+    <h2>Chat</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
