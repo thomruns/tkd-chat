0e55584fa916b541bceeacf35f4953e95429a011
Thom David <thomdavid74@gmail.com>
2018-10-16 10:28:56 -0700

    Set up, style Welcome component

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tkd-chat
 
->A basic chat app using Vue.js and Firebase, based on Sean Pelling Udemy course
+>A basic chat app using Vue.js and Firebase, based on Sean Pelling Udemy course. For simple styling, the app uses utility classes from Materialize, and Google icon fonts.
 
 ## Important
 This app uses Firebase and Firestore. It assumes that you have set up a Firebase database for the project, and created a config file in src/firebase/initi.js, with API keys and other project-specific data from Firebase. Firebase must also be installed as a dependency, as shown in package.json. At the time of writing, Firestore was still in beta.
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="Welcome">
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="indigo-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn indigo">Enter Chat</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +18,28 @@ export default {
   name: 'Welcome',
   data () {
     return {
+      name: null
+    }
+  },
+  methods: {
+    enterChat() {
+      console.log(this.name)
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
 
+.welcome h2 {
+  font-size: 3em;
+}
+
+.welcome button {
+  margin: 30px auto;
+}
 </style>
